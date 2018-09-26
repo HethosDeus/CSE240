3a055691576a4845af99d084f096b140e3f63537
HethosDeus <aclifton237@gmail.com>
2018-09-26 15:51:56 -0700

    Create homework.h

---
+++ homework.h
@@ -0,0 +1,25 @@
+/**
+* Description:This is the header file for homeowork week 4 part 1.  This file
+provides class definitions for the main_part1.cpp and homework.cpp files
+* 
+*
+* @author Adam Clifton
+* @version 2018.09.07
+*/
+#pragma once
+
+
+class Homework {
+
+private:
+	//I don't think there are any private variables
+
+public:
+	void initialize_array(int[], int);
+	void print_array(int[], int);
+	void selection_sort(int[], int);
+	int factorial(int);
+
+};
+
+
