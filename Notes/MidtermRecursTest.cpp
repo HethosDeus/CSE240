63291ee776023c56df3aa638a1349a1b2383b7cd
Adam <aclifton237@gmail.com>
2019-01-30 10:45:12 -0700

    Create MidtermRecursTest.cpp

---
+++ Notes/MidtermRecursTest.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+/*Create a function that print the following. It should be recursive, i.e., 
+DO NOT use for, do/while or while statements here. Output:
+ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1
+*/
+void recursion_up(int num) {  //use recursive call to increment num < 10
+
+	if (num == 10)
+		cout << "0 ";
+
+	if (num < 10) {
+
+		cout << num << " ";
+		recursion_up(num + 1);
+		//cout << num << " ";
+	} 
+}
+
+void recursion_down(int num) {//use recursive call to decrement num >= 1
+
+	if (num >= 1) {
+		cout << num << " ";
+		recursion_down(num - 1);
+		//cout << num << " ";
+	}
+}
+
+void recursion(int num) { // this is a simple way to do it; in one function using symmetry with the cout << calls before and after the recursive step
+	
+	if (num == 10)
+		cout << "0, ";
+	
+	if (num < 10) {
+
+		cout << num << ", ";
+		recursion(num + 1);
+		cout << num << ", ";
+	}
+}
+
+
+/*
+int main() {
+	
+	char a[5] = {'a', 'b', 'c' , '\0'};
+	char *b = a;
+	cout << b;
+	
+	cout << "\nrecursion up: " << endl;
+	recursion_up(1);
+	cout << "\nrecursion down: " << endl;
+	recursion_down(9);
+	cout << "\nrecursion using a single method: " << endl;
+	recursion(1);
+	return 0;
+}*/
